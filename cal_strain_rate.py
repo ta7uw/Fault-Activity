37e0ebc8c533c3cc1472eaadfd5a84ec90d33094
Shimbo Takaaki <ta7uwtaka@gmail.com>
2017-11-25 20:08:22 +0900

    Fix function of calculation

--- cal_strain_rate.py
+++ cal_strain_rate.py
@@ -9,9 +9,13 @@ def normal_f(length, displacement_speed, slope_gradient):
     :param slope_gradient: Slope gradeint of fault
     :return: Crustal strain rate
     """
-    tan = math.tan(math.pi / slope_gradient)
-    strain_rate = length * 0.001 * displacement_speed / tan
-    return round(strain_rate, 4)
+
+    deg = slope_gradient
+    rad = math.radians(deg)
+    tan = math.tan(rad)
+    strain_rate = length * 0.001 * 0.001 * displacement_speed / tan
+    strain_rate = strain_rate / (40 * 40)
+    return strain_rate
 
 
 def slip_f(length, displacement_speed):
@@ -22,7 +26,8 @@ def slip_f(length, displacement_speed):
     :return: Crustal strain rate
     """
     cos = math.cos(math.pi / 4)
-    strain_rate = length * 0.001 * displacement_speed * cos * cos
-    return round(strain_rate, 3)
+    strain_rate = length * 0.001 * 0.001 * displacement_speed * cos * cos
+    strain_rate = strain_rate / (40 * 40)
+    return strain_rate
 
 
