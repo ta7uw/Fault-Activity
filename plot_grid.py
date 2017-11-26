06b06b7780af59703cf0f3f82375ab05dcb118be
Shimbo Takaaki <ta7uwtaka@gmail.com>
2017-11-26 17:35:14 +0900

    Use dataframe by pandas

--- plot_grid.py
+++ plot_grid.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def plot_grid():
@@ -11,26 +12,31 @@ def plot_grid():
     LON_DEGREE = 40 / 93.45286
     LAT_DEGREE = 40 / 111.263
 
-    grid2SHIKOKU = []
+    grid2SHIKOKU = pd.DataFrame(index=[], columns=["center-point", "bbox"])
     for lon in np.arange(start=SHIKOKU_BOX[0], stop=SHIKOKU_BOX[2], step=LON_DEGREE, dtype=float):
-        for lat in np.arange(start=SHIKOKU_BOX[1], stop=SHIKOKU_BOX[3], step=LAT_DEGREE, dtype=float):
-            center_point = lon, lat
-            bbox = (lon - LON_DEGREE/2, lat - LAT_DEGREE/2, lon + LON_DEGREE/2, lat + LAT_DEGREE/2)
-            grid_data = center_point, bbox
-            grid2SHIKOKU.append(grid_data)
 
-    grid2KYUSYU = []
+        for lat in np.arange(start=SHIKOKU_BOX[1], stop=SHIKOKU_BOX[3], step=LAT_DEGREE, dtype=float):
+
+            center_point = [lon, lat]
+            bbox = [lon - LON_DEGREE/2, lat - LAT_DEGREE/2, lon + LON_DEGREE/2, lat + LAT_DEGREE/2]
+
+            grid_data = [center_point, bbox]
+            grid_data = pd.Series(grid_data, index=grid2SHIKOKU.columns)
+
+            grid2SHIKOKU = grid2SHIKOKU.append(grid_data, ignore_index=True)
+
+    grid2KYUSYU = pd.DataFrame(index=[], columns=["center-point", "bbox"])
     for lon in np.arange(start=KYUSYU_BOX[0], stop=KYUSYU_BOX[2], step=LON_DEGREE, dtype=float):
+
         for lat in np.arange(start=KYUSYU_BOX[1], stop=KYUSYU_BOX[3], step=LAT_DEGREE, dtype=float):
+
             center_point = lon, lat
             bbox = (lon - LON_DEGREE / 2, lat - LAT_DEGREE / 2, lon + LON_DEGREE / 2, lat + LAT_DEGREE / 2)
-            grid_data = [center_point, bbox]
-            grid2KYUSYU.append(grid_data)
 
-    print(grid2SHIKOKU)
-    print(grid2KYUSYU)
-    print(len(grid2KYUSYU))
-    print(len(grid2SHIKOKU))
+            grid_data = [center_point, bbox]
+            grid_data = pd.Series(grid_data, index=grid2SHIKOKU.columns)
+
+            grid2KYUSYU = grid2KYUSYU.append(grid_data, ignore_index=True)
 
 if __name__ == '__main__':
     plot_grid()
