7b5589ff56b32c1e2555c235ce1c272968121900
Shimbo Takaaki <ta7uwtaka@gmail.com>
2017-12-26 09:50:59 +0900

    Change plot grid size from 20 km to 10 km

--- plot_grid.py
+++ plot_grid.py
@@ -8,9 +8,9 @@ def plot_grid():
     KYUSYU_BOX = [129.0, 31.0, 132.0, 34.0]
     CHUGOKU_BOX = [130.7, 33.7, 135.0, 35.7]
 
-    # degree per 20km
-    LON_DEGREE = 20 / 93.45286
-    LAT_DEGREE = 20 / 111.263
+    # degree per 20 km -> 10 km
+    LON_DEGREE = 10 / 93.45286  # 20 / 93.45286
+    LAT_DEGREE = 10 / 111.263  # 10 / 111.263
 
     grid2SHIKOKU = pd.DataFrame(index=[], columns=["center-lon", "center-lat", "xmin", "ymin", "xmax", "ymax"])
     for lon in np.arange(start=CHUGOKU_BOX[0], stop=CHUGOKU_BOX[2], step=LON_DEGREE, dtype=float):
@@ -35,8 +35,8 @@ def plot_grid():
             grid_data = pd.Series(grid_data, index=grid2SHIKOKU.columns)
             grid2KYUSYU = grid2KYUSYU.append(grid_data, ignore_index=True)
 
-    grid2KYUSYU.to_csv("csv/grid-KYUSYU.csv", encoding="utf-8")
-    grid2SHIKOKU.to_csv("csv/grid-CHUGOKU.csv",  encoding="utf-8")
+    grid2KYUSYU.to_csv("csv/grid-KYUSYUper10km.csv", encoding="utf-8")
+    grid2SHIKOKU.to_csv("csv/grid-CHUGOKU-per10km.csv",  encoding="utf-8")
 
 if __name__ == '__main__':
     plot_grid()
