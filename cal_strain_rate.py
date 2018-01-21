1b354febacb3c1dcfe17a559f8b9cd91e91cd766
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-01-21 14:06:52 +0900

    Add doc

--- cal_strain_rate.py
+++ cal_strain_rate.py
@@ -4,10 +4,10 @@ import math
 def normal_f(length, displacement_speed, slope_gradient):
     """
     Calculatate horizontal strain rate of normal fault and reverse fault
-    :param length: Lenghth of fault
-    :param displacement_speed: Displacement speed of fault
-    :param slope_gradient: Slope gradeint of fault
-    :return: Crustal strain rate
+    :param length: Lenghth of fault (km)
+    :param displacement_speed: Displacement speed of fault (m/10^3year)
+    :param slope_gradient: Slope gradeint of fault (degree)
+    :return: Crustal strain rate (/year)
     """
 
     deg = slope_gradient
@@ -21,9 +21,9 @@ def normal_f(length, displacement_speed, slope_gradient):
 def slip_f(length, displacement_speed):
     """
     Calcultate horizontal strain rate of strike slip fault
-    :param length: Lenghth of fault
-    :param displacement_speed: Displacement speed of fault
-    :return: Crustal strain rate
+    :param length: Lenghth of fault (km)
+    :param displacement_speed: Displacement speed of fault (m/10^3year)
+    :return: Crustal strain rate (/year)
     """
     cos = math.cos(math.pi / 4)
     strain_rate = length * 0.001 * 0.001 * displacement_speed * cos * cos
