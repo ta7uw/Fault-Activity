3c508a88cddd1b9d9a685037445c57a4490ef169
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-02-02 19:28:53 +0900

    fix: a bug fix

--- DMS2degree_epicenter.py
+++ DMS2degree_epicenter.py
@@ -16,18 +16,18 @@ def dms2degree_epicenter():
     print(data_size)
 
     for i in range(0, data_size):
-        lat_1 = df.loc[i][4].split("°")[0]
-        lon_1 = df.loc[i][3].split("°")[0]
+        lat_1 = df.loc[i][4].split("°")
+        lon_1 = df.loc[i][3].split("°")
 
-        lat_2 = df.loc[i][4].split(".")[0]
-        lon_2 = df.loc[i][3].split(".")[0]
+        lat_2 = lat_1[1].split(".")
+        lon_2 = lon_1.split(".")
+        print(lon_2)
 
-        lat_3 = df.loc[i][4].split("'")[0]
-        lon_3 = df.loc[i][3].split("'")[0]
+        lat_3 = lat_2.split("′")
+        lon_3 = lon_2.split("′")
 
         lat = int(lat_1[0]) + int(lat_2[0])/60 + int(lat_3[0])/3600
         lon = int(lon_1[0]) + int(lon_2[0])/60 + int(lon_3[0])/3600
-        print(lon)
 
         df.loc[i][3] = lat
         df.loc[i][4] = lon
